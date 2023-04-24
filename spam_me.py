c0b0617d5c7d8540e79941ebe0bae247738a7bf5
olizimmermann <github@ozimmermann.com>
2023-04-24 20:15:05 +0200

    update with command line params

--- spam_me.py
+++ spam_me.py
@@ -5,18 +5,8 @@ import requests
 import random
 import string
 import pathlib
+import argparse
 import urllib.parse
-
-base_url = "https://api.telegram.org/bot"
-bot_id = ''
-token = ''
-chat_id = ''
-
-name_list_path = pathlib.Path('/usr/share/wordlists/SecLists/Usernames/Names/names.txt')
-family_name_list_path = pathlib.Path('/usr/share/wordlists/SecLists/Usernames/Names/familynames-usa-top1000.txt')
-domain_list_path = pathlib.Path('/usr/share/wordlists/email_provider.txt')
-city_list_path = pathlib.Path('/usr/share/wordlists/SecLists/Miscellaneous/us-cities.txt')
-
 
 def get_list(path):
     wordlist = []
@@ -25,16 +15,11 @@ def get_list(path):
             wordlist.append(line.replace('\n',''))
     return wordlist
 
-name_list = get_list(name_list_path)
-family_name_list = get_list(family_name_list_path)
-city_list = get_list(city_list_path)
-domain_list = get_list(domain_list_path)
-
 def random_pw(s=8, e=12):
     n = random.randint(s,e)
     return ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=n))
 
-def random_email(name, family_name, domain=domain_list):
+def random_email(name, family_name, domain_list):
     return f'{random.choice(name).lower()}.{random.choice(family_name).lower()}@{random.choice(domain_list)}'
 
 def send_message(msg):
@@ -45,37 +30,95 @@ def send_message(msg):
     else:
         return r.status_code
 
-def create_msg():
-    P_EMAIL = random_email(name_list,family_name_list)
+def create_msg(text=None):
+    P_EMAIL = random_email(name_list,family_name_list, domain_list)
     P_PASSWORD = random_pw()
     P_IP = f"{random.randint(11,230)}.{random.randint(11,230)}.{random.randint(11,230)}.{random.randint(11,230)}"
     P_ZIP = random.randint(1000,88999)
     P_COUNTRYCODE = "".join(random.choices(string.ascii_uppercase) + random.choices(string.ascii_uppercase))
     P_CITY = random.choice(city_list)
-    P_GECKOVERSION = str(random.randint(20000000,22000000))
+    P_RANDHIGHINT = str(random.randint(20000000,22000000))
     P_ORG = random.choice(family_name_list) + "-" + random.choice(family_name_list)
     
-    text = """<b>OFFICE365-HTML-LOGS@ZERO</b>
+
+    default = """<b>OFFICE365-HTML-LOGS@ZERO</b>
     [1] 24/04/2023
-    <b>USER-AGENT: </b>Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/P_GECKOVERSION Firefox/105.0
+    <b>USER-AGENT: </b>Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/P_RANDHIGHINT Firefox/105.0
     <a>seemee: @pwned</a>
     <b>EMAIL: </b><pre>P_EMAIL</pre>
     <b>PASSWORD: </b><a>P_PASSWORD</a>
     <b>Location: </b>IP: P_IP | CITY: P_CITY | COUNTRY: P_COUNTRYCODE | ORG: P_ORG | POSTAL: P_ZIP&parse_mode=html HTTP/2"""
 
-    text = text.replace('P_EMAIL', P_EMAIL).replace('P_PASSWORD', P_PASSWORD).replace('P_IP', P_IP).replace('P_ZIP', str(P_ZIP)).replace('P_COUNTRYCODE', P_COUNTRYCODE).replace('P_CITY', P_CITY).replace('P_GECKOVERSION', P_GECKOVERSION).replace("P_ORG", P_ORG)
+    if text is None:
+        text = default
+
+    text = text.replace('P_EMAIL', P_EMAIL).replace('P_PASSWORD', P_PASSWORD).replace('P_IP', P_IP).replace('P_ZIP', str(P_ZIP)).replace('P_COUNTRYCODE', P_COUNTRYCODE).replace('P_CITY', P_CITY).replace('P_RANDHIGHINT', P_RANDHIGHINT).replace("P_ORG", P_ORG)
 
 
     text = urllib.parse.quote_plus(text)
     
     return text, P_EMAIL
 
-max_msgs = 100000
+
+print('Starting mission: Phishers Attack')
+parser = argparse.ArgumentParser(description="This bot is made to fight against telegram phishing bots")
+parser.add_argument('--bot-id', type=str, help="ID of telegram bot", required=True)
+parser.add_argument('--token', type=str, help="Token of telegram bot", required=True)
+parser.add_argument('--chat-id', type=str, help="Chat ID", required=True)
+parser.add_argument('--messages', type=int, help="Amount of messages", default=1, required=False)
+parser.add_argument('--text', type=str, help="Define own message to send. Will be url encoded. Use variables inside to make it random: P_IP|_EMAIL|P_PASSWORD|P_ZIP|P_CITY|P_RANDHIGHINT|P_ORG", required=False, default=None)
+
+parser.add_argument('--disable-check', help="Disable connectivity check", required=False, action='store_true')
+parser.add_argument('--name-list', type=str, default='/usr/share/wordlists/SecLists/Usernames/Names/names.txt', help="First Names Wordlist")
+parser.add_argument('--surname-list', type=str, default='/usr/share/wordlists/SecLists/Usernames/Names/familynames-usa-top1000.txt', help="First Names Wordlist")
+parser.add_argument('--domain-list', type=str, default='/usr/share/wordlists/email_provider.txt', help="Domains Wordlist")
+parser.add_argument('--city-list', type=str, default='/usr/share/wordlists/SecLists/Usernames/Names/names.txt', help="City Wordlist")
+
+args = parser.parse_args()
+base_url = "https://api.telegram.org/bot"
+bot_id = args.bot_id
+token = args.token
+chat_id = args.chat_id
+max_msgs = args.messages
+
+if not args.disable_check:
+    # ip check 
+    r = requests.get('https://api.ipify.org')
+    if r.ok:
+        print(f'The public IP address which is used: {r.text}')
+        print('...')
+        time.sleep(5)
+    else:
+        print('No Connection! Please check your internet connection. Ignore connectivity test with: --disable-check')
+        sys.exit(0)
+
+if not os.path.isfile(args.name_list):
+    print(f'Please define wordlist path for: --name-list')
+    sys.exit(0)
+if not os.path.isfile(args.surname_list):
+    print(f'Please define wordlist path for: --surname-list')
+    sys.exit(0)
+if not os.path.isfile(args.domain_list):
+    print(f'Please define wordlist path for: --domain-list')
+    sys.exit(0)
+if not os.path.isfile(args.city_list):
+    print(f'Please define wordlist path for: --city-list')
+    sys.exit(0)
+
+name_list_path = pathlib.Path(args.name_list)
+family_name_list_path = pathlib.Path(args.surname_list)
+domain_list_path = pathlib.Path(args.domain_list)
+city_list_path = pathlib.Path(args.city_list)
+name_list = get_list(name_list_path)
+family_name_list = get_list(family_name_list_path)
+city_list = get_list(city_list_path)
+domain_list = get_list(domain_list_path)
+
 for i in range(max_msgs):
-    txt, email = create_msg()
+    txt, email = create_msg(args.text)
     ret = send_message(txt)
-    time.sleep(random.randint(1,5))
-    print(f'Sending {i}/{max_msgs} {ret} from {email}')
+    time.sleep(random.randint(1,4))
+    print(f'Sending {i}/{max_msgs} {ret} from E-Mail: {email}')
     if ret > 203:
         print(f'Bot pwned after {i} messages.')
         sys.exit(0)
